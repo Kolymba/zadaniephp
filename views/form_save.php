cd30032f35c9266eb9a5712eebfd42f4497aa162
Kolymba <nickolay.isaeff@gmail.com>
2021-06-28 20:12:54 +0500

    main

--- views/form_save.php
+++ views/form_save.php
@@ -3,7 +3,7 @@
     <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="main.css" /> 
+    <link rel="stylesheet" type="text/css" href="css/main.css" /> 
     <title>Редактирование перевода</title>
     <!--[if IE]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
@@ -12,10 +12,11 @@
     <body>
         <div class="container">
             <div class="wrapper">
-                <div class="section">
-                    <?php   
-                        echo '<div>Форма сохранена</div>';                                             
-                    ?>
+                <div class="section2">                    
+                    <div class="success">
+                        <div>Форма сохранена</div> 
+                        <input type="button" value="Назад" class="homebutton" id="btnHome" onClick="window.location = 'index.php'" />
+                    </div>                   
                 </div> 
             </div>                 
         </div>
--- views/index.php
+++ views/index.php
@@ -3,7 +3,7 @@
     <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="main.css" /> 
+    <link rel="stylesheet" type="text/css" href="css/main.css" /> 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js" type="text/javascript"></script>
     <title>Редактирование перевода</title>
     <!--[if IE]>
